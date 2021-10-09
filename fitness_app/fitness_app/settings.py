058fcc29823eaf706846a2b5b872fd618cdca568
Macm91 <macm91.mc@gmail.com>
2021-10-08 22:28:41 -0400

    created views in authentication and serializers.

--- fitness_app/fitness_app/settings.py
+++ fitness_app/fitness_app/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'authentication.apps.AuthenticationConfig',
+    
 ]
 
 MIDDLEWARE = [
@@ -118,6 +121,12 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES' : (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        )
+}
 
 try: 
     from fitness_app.local_settings import*
