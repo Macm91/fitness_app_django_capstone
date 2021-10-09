474407accb0b3d4457df4646350d111185cd2029
Macm91 <macm91.mc@gmail.com>
2021-10-08 22:15:05 -0400

    made migrations, migrated to database. Updated settings.py to go to local setting

--- fitness_app/fitness_app/settings.py
+++ fitness_app/fitness_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-shvko2cm9*u5)220a&^itiqh*2$ho!(%v07#8pui!j)#bdf@6t'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -73,12 +73,7 @@ WSGI_APPLICATION = 'fitness_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # Password validation
@@ -123,3 +118,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try: 
+    from fitness_app.local_settings import*
+except ImportError:
+    pass
