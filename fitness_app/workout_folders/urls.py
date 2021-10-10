7bb72c1e251730a5782fb3c02c7617b1b6a7e86d
Macm91 <macm91.mc@gmail.com>
2021-10-10 13:41:28 -0400

    redid the folder and exercise methods to allow for JWT tokens and better filteringfor user folders

--- fitness_app/workout_folders/urls.py
+++ fitness_app/workout_folders/urls.py
@@ -3,37 +3,42 @@ from django.urls.resolvers import URLPattern
 from rest_framework import views
 
 # from fitness_app.WorkoutFolder.models import WorkoutExercises
-from .views import ExerciseList
-from .views import WorkoutExercisesList
-from .views import WorkoutList
+from .views import add_exercise, get_all_exrecises
+# from .views import WorkoutExercisesList
+# from .views import WorkoutList
 from . import views
 
 urlpatterns = [
 
-    # Exercises
-    path('exercise/', views.ExerciseList.as_view()),
+# Exercises
+    # path('exercise/', views.ExerciseList.as_view()),
+    path('exercises/', views.get_all_exrecises),
+    path('addexercise/', views.add_exercise),
+    path('folders/', views.user_folders),
+    
 
-    # WorkoutExercises
-    path('workoutexercises/', views.WorkoutExercisesList.as_view(), name='get'),
+
+# WorkoutExercises
+    # path('workoutexercises/', views.WorkoutExercisesList.as_view(), name='get'),
    
-#    ####################################
-    # Above = Confirmed endpoints. Below, not so much. 
-    # ################################### 
+
+
+
 
  # WorkoutExercises
     # get details on just one workoutExercise
-    path('workoutexercises/<int:pk>/', views.WorkoutExercisesList.as_view(), name = 'get_object'),
-    # Edit a workoutexercise 
-    path('workoutexercises/edit/<int:w_e_id>/', views.WorkoutExercisesList.as_view(), name='edit'),
+#     path('workoutexercises/<int:pk>/', views.WorkoutExercisesList.as_view(), name = 'get_object'),
+#     # Edit a workoutexercise 
+#     path('workoutexercises/edit/<int:w_e_id>/', views.WorkoutExercisesList.as_view(), name='edit'),
 
-# Workout 
-    # Get all workouts
-    path('workout/', views.WorkoutList.as_view(), name='get'),
+# # Workout 
+#     # Get all workouts
+#     path('workout/', views.WorkoutList.as_view(), name='get'),
   
 
-# workout Folder
-    # get all folders
-    path('folders/', views.WorkoutFolderList.as_view()),
+# # workout Folder
+#     # get all folders
+#     path('folders/', views.WorkoutFolderList.as_view()),
 
 
 ]
--- fitness_app/workout_folders/views.py
+++ fitness_app/workout_folders/views.py
@@ -18,135 +18,183 @@ from django.shortcuts import render
 
 
 
-class ExerciseList(APIView):
+# exercise methods
 
-    permission_classes = [AllowAny]
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_all_exrecises(request):
+    exercises = Exercise.objects.all()
+    serializers = ExerciseSerializer(exercises, many=True)
+    return Response(serializers.data)
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def add_exercise(request):
+    serializers=ExerciseSerializer(data=request.data)
+    if serializers.is_valid():
+        serializers.save()
+        return Response(serializers.data, status=status.HTTP_201_CREATED)
+    return Response(serializers.errors, status= status.HTTP_400_BAD_REQUEST)
+
+
+#folder methods
+
+
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def user_folders(request):
+
+    print(
+        'User', f"{request.user.id}{request.user.email}{request.user.username}"
+    )
+
+    if request.method == 'POST':
+        serializer = WorkoutFolderSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status= status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'GET':
+        folder = WorkoutFolder.objects.filter(user_id=request.user.id)
+        serializer = WorkoutFolderSerializer(folder, many=True)
+        return Response(serializer.data)
+
+
+
+
+
+
+
+# class ExerciseList(APIView):
+
+#     permission_classes = [AllowAny]
     
-    def get(self, request):
-        exercise = Exercise.objects.all()
-        serializer = ExerciseSerializer(exercise, many=True)
-        return Response(serializer.data)
+#     def get(self, request):
+#         exercise = Exercise.objects.all()
+#         serializer = ExerciseSerializer(exercise, many=True)
+#         return Response(serializer.data)
 
 
-    def post (self, request):
-        serializers=ExerciseSerializer(data=request.data)
-        if serializers.is_valid():
-            serializers.save()
-            return Response(serializers.data, status=status.HTTP_201_CREATED)
-        return Response(serializers.errors, status= status.HTTP_400_BAD_REQUEST)
-
-
-
-class WorkoutExercisesList(APIView):
-
-    permission_classes =[AllowAny]
-
-    def get(self, request):
-        w_e = WorkoutExercises.objects.all()
-        serializer = WorkoutExercisesSerializer(w_e, many=True)
-        return Response(serializer.data)
-
-    def post (self, request):
-        serializers=WorkoutExercisesSerializer(data=request.data)
-        if serializers.is_valid():
-            serializers.save()
-            return Response(serializers.data, status=status.HTTP_201_CREATED)
-        return Response(serializers.errors, status= status.HTTP_400_BAD_REQUEST)
-
-
-    # def details (self, pk):
-    #     w_e = WorkoutExercises.objects.get(pk = pk)
-    #     context = {
-    #         'w_e' : w_e
-    #     }
-    #     return render( 'WorkoutExercises/details.html', context)
+#     def post (self, request):
+#         serializers=ExerciseSerializer(data=request.data)
+#         if serializers.is_valid():
+#             serializers.save()
+#             return Response(serializers.data, status=status.HTTP_201_CREATED)
+#         return Response(serializers.errors, status= status.HTTP_400_BAD_REQUEST)
 
 
 
-    def get_object(request ,pk):
-        try:
-            w_e = WorkoutExercises.objects.get(pk=pk)
-            serializer = WorkoutExercisesSerializer(w_e, many=True)
-            return Response(serializers.data)
-        except WorkoutExercises.DoesNotExist:
-            raise Response(serializers.errors, status= status.HTTP_400_BAD_REQUEST)
+# class WorkoutExercisesList(APIView):
+
+#     permission_classes =[AllowAny]
+
+#     def get(self, request):
+#         w_e = WorkoutExercises.objects.all()
+#         serializer = WorkoutExercisesSerializer(w_e, many=True)
+#         return Response(serializer.data)
+
+#     def post (self, request):
+#         serializers=WorkoutExercisesSerializer(data=request.data)
+#         if serializers.is_valid():
+#             serializers.save()
+#             return Response(serializers.data, status=status.HTTP_201_CREATED)
+#         return Response(serializers.errors, status= status.HTTP_400_BAD_REQUEST)
 
 
-    # def edit_w_e(self,request,pk):
-    #         w_e = self.get_object(pk)
-    #         serializer = WorkoutExercisesSerializer(w_e, data=request.data)
-    #         if serializer.is_valid():
-    #             serializer.save()
-    #             return Response(serializer.data)
-    #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     # def details (self, pk):
+#     #     w_e = WorkoutExercises.objects.get(pk = pk)
+#     #     context = {
+#     #         'w_e' : w_e
+#     #     }
+#     #     return render( 'WorkoutExercises/details.html', context)
+
+
+
+#     def get_object(request ,pk):
+#         try:
+#             w_e = WorkoutExercises.objects.get(pk=pk)
+#             serializer = WorkoutExercisesSerializer(w_e, many=True)
+#             return Response(serializers.data)
+#         except WorkoutExercises.DoesNotExist:
+#             raise Response(serializers.errors, status= status.HTTP_400_BAD_REQUEST)
+
+
+#     # def edit_w_e(self,request,pk):
+#     #         w_e = self.get_object(pk)
+#     #         serializer = WorkoutExercisesSerializer(w_e, data=request.data)
+#     #         if serializer.is_valid():
+#     #             serializer.save()
+#     #             return Response(serializer.data)
+#     #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
  
-    # def edit(request, w_e_id):
-    #     w_e= WorkoutExercises.objects.get(pk=w_e_id)
-    #     if request.method== 'POST':
-    #         w_e.exercise_id = request.POST.get('exercise_id')
-    #         w_e.sets = request.POST.get('sets')
-    #         w_e.reps = request.POST.get('reps')
-    #         w_e.notes = request.POST.get('notes')
-    #         w_e.save()
-    #         return HttpResponse (reverse('WorkoutFolder: all_exercises_in_workout'))
-    #     else:
-    #         context ={
-    #             'w_e' : w_e
-    #         }
-    #         return render (request, 'WorkoutFolder/edit.html', context)
+#     # def edit(request, w_e_id):
+#     #     w_e= WorkoutExercises.objects.get(pk=w_e_id)
+#     #     if request.method== 'POST':
+#     #         w_e.exercise_id = request.POST.get('exercise_id')
+#     #         w_e.sets = request.POST.get('sets')
+#     #         w_e.reps = request.POST.get('reps')
+#     #         w_e.notes = request.POST.get('notes')
+#     #         w_e.save()
+#     #         return HttpResponse (reverse('WorkoutFolder: all_exercises_in_workout'))
+#     #     else:
+#     #         context ={
+#     #             'w_e' : w_e
+#     #         }
+#     #         return render (request, 'WorkoutFolder/edit.html', context)
 
-    def delete(self, pk):
-        w_e = self.get_object(pk)
-        w_e.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+#     def delete(self, pk):
+#         w_e = self.get_object(pk)
+#         w_e.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
-    # def delete(request, w_e_id):
-    #     delete_workout_exercise = WorkoutExercises.objects.get(pk = w_e_id)
-    #     if request.method == 'POST':
-    #         WorkoutExercises.objects.filter(pk= w_e_id).delete()
+#     # def delete(request, w_e_id):
+#     #     delete_workout_exercise = WorkoutExercises.objects.get(pk = w_e_id)
+#     #     if request.method == 'POST':
+#     #         WorkoutExercises.objects.filter(pk= w_e_id).delete()
 
-    #         return HttpResponseRedirect(reverse('WorkoutExercises:get'))
-    #     else:
-    #         context = {
-    #             'delete_workout_exercise': delete_workout_exercise
-    #         }
-    #         return render(request, 'workoutExercises/delete.html', context)
+#     #         return HttpResponseRedirect(reverse('WorkoutExercises:get'))
+#     #     else:
+#     #         context = {
+#     #             'delete_workout_exercise': delete_workout_exercise
+#     #         }
+#     #         return render(request, 'workoutExercises/delete.html', context)
 
 
     
 
-class WorkoutList(APIView):
+# class WorkoutList(APIView):
     
-    permission_classes = [AllowAny]
+#     permission_classes = [AllowAny]
 
-    def get(self, request):
-        workout = Workout.objects.all()
-        serializer = WorkoutSerializer(workout, many=True)
-        return Response(serializer.data)
+#     def get(self, request):
+#         workout = Workout.objects.all()
+#         serializer = WorkoutSerializer(workout, many=True)
+#         return Response(serializer.data)
 
-    def post (self, request):
-        serializers=WorkoutSerializer(data=request.data)
-        if serializers.is_valid():
-            serializers.save()
-            return Response(serializers.data, status=status.HTTP_201_CREATED)
-        return Response(serializers.errors, status= status.HTTP_400_BAD_REQUEST)
-
-
+#     def post (self, request):
+#         serializers=WorkoutSerializer(data=request.data)
+#         if serializers.is_valid():
+#             serializers.save()
+#             return Response(serializers.data, status=status.HTTP_201_CREATED)
+#         return Response(serializers.errors, status= status.HTTP_400_BAD_REQUEST)
 
 
-class WorkoutFolderList(APIView):
 
-    permission_classes =[AllowAny]
 
-    def get(self, request):
-        w_f = WorkoutFolder.objects.all()
-        serializer = WorkoutFolderSerializer(w_f, many=True)
-        return Response(serializer.data)
+# class WorkoutFolderList(APIView):
 
-    def post (self, request):
-        serializers=WorkoutFolderSerializer(data=request.data)
-        if serializers.is_valid():
-            serializers.save()
-            return Response(serializers.data, status=status.HTTP_201_CREATED)
-        return Response(serializers.errors, status= status.HTTP_400_BAD_REQUEST)
+#     permission_classes =[AllowAny]
+
+#     def get(self, request):
+#         w_f = WorkoutFolder.objects.all()
+#         serializer = WorkoutFolderSerializer(w_f, many=True)
+#         return Response(serializer.data)
+
+#     def post (self, request):
+#         serializers=WorkoutFolderSerializer(data=request.data)
+#         if serializers.is_valid():
+#             serializers.save()
+#             return Response(serializers.data, status=status.HTTP_201_CREATED)
+#         return Response(serializers.errors, status= status.HTTP_400_BAD_REQUEST)
 
