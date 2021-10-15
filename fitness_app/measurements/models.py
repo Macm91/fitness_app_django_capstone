90021821d633b37ccde08a9889d1e1c8ba787036
Macm91 <macm91.mc@gmail.com>
2021-10-15 17:22:47 -0400

    Created workout history model in workoutfolder

--- fitness_app/measurements/models.py
+++ fitness_app/measurements/models.py
@@ -6,5 +6,5 @@ class Measurements(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, default=1)
     body_part = models.CharField(max_length=30)
     measurement = models.IntegerField()
-    date = models.DateField()
+    date = models.DateField(auto_now=True)
 
--- fitness_app/workout_folders/models.py
+++ fitness_app/workout_folders/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.base import Model
 from django.db.models.deletion import CASCADE
 from django.db.models.expressions import F
 
@@ -31,3 +32,7 @@ class WorkoutExercises(models.Model):
     notes = models.CharField(max_length=100, null=False, blank=True) 
     
    
+class WorkoutHistory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    workout = models.ForeignKey(Workout, on_delete=models.PROTECT)
+    date = models.DateField(auto_now=True)
